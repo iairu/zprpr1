e2415f6938c1432ebe691855fe55b67e62e6c7b9
azurry <theazurry@gmail.com>
2019-11-19 23:02:07 +0100

    vyhodenie poctovych premennych, implementacia '\0'

--- - Projekt/sifrovanie.c
+++ - Projekt/sifrovanie.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int nacitanie_textu(char *p, int n, int *pn) {
+int nacitanie_textu(char *p, int n) {
 	FILE *fr;
 	int i = 0;	  // poradove cislo momentalne nacitaneho znaku
 	char c;		  // momentalny znak v subore
-	int pocet_znakov = 0;
 	if ((fr = fopen("sifra.txt", "r")) == NULL) {
 		printf("Spravu sa nepodarilo nacitat\n");
 		return 0;
@@ -18,18 +17,17 @@ int nacitanie_textu(char *p, int n, int *pn) {
 	}
 	while ((c = getc(fr)) != EOF && i <= n) {
 		p[i] = c;
-		pocet_znakov++;
 		i++;
 	}
+	p[i] = '\0';
 	fclose(fr);
-	*pn = pocet_znakov;
 	return 0;
 }
 
-int vypis_povodny(char *p, int pn) {
+int vypis_povodny(char *p) {
 	int i;
-	if (pn) {
-		for(i=0;i<pn;i++)
+	if (p[0] != '\0') {
+		for(i=0;p[i]!='\0';i++)
 			putchar(p[i]);
 		putchar('\n');
 	}
@@ -40,11 +38,11 @@ int vypis_povodny(char *p, int pn) {
 	return 0;
 }
 
-int uprava_textu(char *p, char *u, int pn, int *un) {
+int uprava_textu(char *p, char *u) {
 	int i;
 	int pocet_znakov = 0;
-	if (pn) {
-		for(i=0;i<pn;i++) {
+	if (p[0] != '\0') {
+		for(i=0;p[i]!='\0';i++) {
 			if (p[i] >= 'A' && p[i] <= 'Z') {
 				u[pocet_znakov] = p[i];
 				pocet_znakov++;
@@ -59,14 +57,14 @@ int uprava_textu(char *p, char *u, int pn, int *un) {
 		printf("Sprava nie je nacitana\n");
 		return 0;
 	}
-	*un = pocet_znakov;
+	u[pocet_znakov] = '\0';
 	return 0;
 }
 
-int vypis_sifrovany(char* u, int un) {
+int vypis_sifrovany(char* u) {
 	int i;
-	if(un) {
-		for(i=0;i<un;i++) {
+	if(u[0] != '\0') {
+		for(i=0;u[i]!='\0';i++) {
 			putchar(u[i]);
 		}
 		putchar('\n');
@@ -77,12 +75,12 @@ int vypis_sifrovany(char* u, int un) {
 	return 0;
 }
 
-int vypis_slov_dlzky(char* p, int pn) {
+int vypis_slov_dlzky(char* p) {
 	int k;
 	int x;
 	char *q = p;
 	char *r;
-	if (pn == 0) {
+	if (*q == '\0') {
 		printf("Sprava nie je nacitana\n");
 		return 0;
 	}
@@ -90,7 +88,7 @@ int vypis_slov_dlzky(char* p, int pn) {
 	if (k >= 1 && k <= 100) {
 		x = 1;
 		while(1) {
-			if(*q == ' ' || pn == q-p || *q == '\n') {
+			if(*q == ' ' || *q == '\0' || *q == '\n') {
 				if(x == k+1){
 					/* ked sa najde slovo danej dlzky */
 					for(r=q-k;r<q;r++)
@@ -99,7 +97,7 @@ int vypis_slov_dlzky(char* p, int pn) {
 				}
 				x = 0;
 			}
-			if(pn == q-p)
+			if(*q == '\0')
 				break;
 			x++;
 			q++;
@@ -108,13 +106,13 @@ int vypis_slov_dlzky(char* p, int pn) {
 	return 0;
 }
 
-int vypis_histogramu(char* u, int un) {
+int vypis_histogramu(char* u) {
 	int i;
 	int n = 26;
 	char c[n+1];
 	char *q = u;
 	char *r = c;
-	if (un == 0) {
+	if (*q == '\0') {
 		printf("Nie je k dispozicii upravena sprava\n");
 		return 0;
 	}
@@ -153,19 +151,19 @@ int rozsifrovanie() {
 
 int main() {
 	int n = 1000; // max. pocet znakov
-	char p[n+1]; // povodny text
-	char u[n+1]; // upraveny text
+	char p[n]; // povodny text
+	char u[n]; // upraveny text
 	char c; // uzivatelsky vstup
-	int pn = 0; // pocet povodnych znakov
-	int un = 0; // pocet upravenych znakov
+	p[0] = '\0';
+	u[0] = '\0';
 	while((c = getchar()) != 'k') {
 		switch (c) {
-			case 'n': nacitanie_textu(p,n,&pn); break;
-			case 'v': vypis_povodny(p, pn); 	break;
-			case 'u': uprava_textu(p,u,pn,&un); break;
-			case 's': vypis_sifrovany(u, un);	break;
-			case 'd': vypis_slov_dlzky(p,pn);	break;
-			case 'h': vypis_histogramu(u,un);	break;
+			case 'n': nacitanie_textu(p,n); 	break;
+			case 'v': vypis_povodny(p); 		break;
+			case 'u': uprava_textu(p,u);		break;
+			case 's': vypis_sifrovany(u);		break;
+			case 'd': vypis_slov_dlzky(p);		break;
+			case 'h': vypis_histogramu(u);		break;
 			case 'c': rozsifrovanie();			break;
 			case 'k': return 0;				
 			default : break;				
